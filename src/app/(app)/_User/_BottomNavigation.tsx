62ea176749bd65d90e27e3e5b51db0dae5de9ec0
ikkikki <50198675+ikkikki@users.noreply.github.com>
2024-10-05 23:18:50 +0900

    update

--- src/app/(app)/_User/_BottomNavigation.tsx
+++ src/app/(app)/_User/_BottomNavigation.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
